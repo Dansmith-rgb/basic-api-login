6a1f5a4b2f5a0344eb38ec8bcb4db16bcd10218a
Dan <68809853+Dansmith-rgb@users.noreply.github.com>
2020-12-12 16:30:57 +0000

    Add files via upload

--- myapi/admin.py
+++ myapi/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import Register, Signin
 
-admin.site.register(User)
+admin.site.register(Register)
+admin.site.register(Signin)
